e2c8c1d5392c5566a20c6c32514f7d75fdea7f5c
jayoung.FE <68357066+jayoung-cinnamon@users.noreply.github.com>
2022-04-01 19:47:32 +0900

    add firebase

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -3,7 +3,6 @@ import firebase from "firebase/compat/app";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DB_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGE_IN_ID,
